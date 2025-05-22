8babb4bf5ae2d0899fcffe2c6611793405b3d6dd
hst098 <sithang2k2@gmail.com>
2025-05-22 14:09:39 +0700

    fix

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import PageMeta from '@/components/common/PageMeta'
 import Sidebar from '@/features/dashboard/components/SideBar/Sidebar'
 import { SidebarProvider } from '@/features/dashboard/context/SidebarContext'
 import Backdrop from '@/features/dashboard/components/SideBar/Backdrop'
-import Header from '@/features/dashboard/components/Header'
+import Header from '@/features/dashboard/components/Header/Header'
 
 export default function DashboardLayout({
   children,
